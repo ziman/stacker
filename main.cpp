6a3606cb4cbb242dc11f8783cf5cdedc22ae9fe7
Matus Tejiscak <ziman@functor.sk>
2011-06-28 20:27:08 +0200

    Blob detection.

--- main.cpp
+++ main.cpp
@@ -6,18 +6,12 @@
 #include "cvblob.h"
 
 using namespace cv;
+using namespace cvb;
 using namespace std;
 
 struct Options
 {
 	int threshold;
-
-	// StarDetector options
-	int maxSize;
-	int responseThreshold;
-	int lineThresholdProjected;
-	int lineThresholdBinarized;
-	int suppressNonmaxSize;
 };
 
 void die(const string & msg)
@@ -28,32 +22,30 @@ void die(const string & msg)
 
 void getStars(const vector<string> & fn, vector<vector<KeyPoint> *> & stars, const Options & opt)
 {
-	namedWindow("Preview", CV_WINDOW_AUTOSIZE);
 	stars.clear();
 	for (vector<string>::const_iterator it = fn.begin(); it != fn.end(); ++it)
 	{
-		cout << "  * " << *it << ": ";
+		cout << "  * " << *it << endl;
 
 		// load the image
 		Mat srcimg = imread(*it, CV_LOAD_IMAGE_GRAYSCALE);
 		Mat image;
-		threshold(srcimg, image, opt.threshold, 255, THRESH_TOZERO);
+		threshold(srcimg, image, opt.threshold, 255, THRESH_BINARY);
 
 		// find the stars
-		vector<KeyPoint> * pts = new vector<KeyPoint>();
-		StarDetector(
-			opt.maxSize, opt.responseThreshold, opt.lineThresholdProjected,
-			opt.lineThresholdBinarized, opt.suppressNonmaxSize
-		)(image, *pts);
+		CvBlobs blobs;
+		IplImage img = image;
+		IplImage * label = cvCreateImage(cvGetSize(&img), IPL_DEPTH_LABEL, 1);
+		cvLabel(&img, label, blobs);
+		cvReleaseImage(&label);
 
-		// push the vector
-		stars.push_back(pts);
-		cout << pts->size() << " stars." << endl;
-
-		// show the image
-		//drawKeypoints(image, *pts, image, Scalar::all(-1), DrawMatchesFlags::DRAW_OVER_OUTIMG);
-		imshow("Preview", image);
-		waitKey(1); // gui event loop
+		// traverse the blobs
+		int i = 0;
+		for (CvBlobs::iterator it = blobs.begin(); it != blobs.end(); ++it)
+		{
+			++i;
+		}
+		cout << i << endl;
 	}
 }
 
@@ -64,12 +56,7 @@ int main(int argc, char ** argv)
 
 	// some default options
 	Options opt;
-	opt.maxSize = 32;
-	opt.responseThreshold = 10;
-	opt.lineThresholdProjected = 8;
-	opt.lineThresholdBinarized = 8;
-	opt.suppressNonmaxSize = 0;
-	opt.threshold = 32;
+	opt.threshold = 48;
 
 	// get the options
 	while (argv < end)
