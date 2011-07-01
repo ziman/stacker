ad9923e9c1b88883b3726e5315615d4d5e7dbd6b
Matus Tejiscak <ziman@functor.sk>
2011-07-01 05:46:15 +0200

    Segment counting fixed.

--- main.cpp
+++ main.cpp
@@ -78,94 +78,8 @@ inline double min3(double x, double y, double z)
 	return (x < y) ? ((x < z) ? x : z) : ((y < z) ? y : z);
 }
 
-#define D(m,n) dist[N*m + n]
-#define C(m,n) cost[NN*m + n]
 void getTransform(Stars & xs, Stars & ys)
 {
-	int M = xs.size();
-	int N = ys.size();
-
-	// distance table
-	double distSum = 0;
-	double * dist = new double[M*N];
-	for (int m = 0; m < M; ++m)
-		for (int n = 0; n < N; ++n)
-		{
-			D(m,n) = sqrt(
-				  sqr(xs[m].x - ys[n].x)
-				+ sqr(xs[m].y - ys[n].y)
-			);
-			distSum += D(m,n);
-		}
-	double avgDist = distSum / (M*N);
-
-	// calculate the cost table
-	int MM = M+1, NN = N+1;
-	double * cost = new double[MM*NN];
-	for (int m = 0; m <= M; ++m) C(m,0) = 0;
-	for (int n = 0; n <= N; ++n) C(0,n) = 0;
-	for (int m = 1; m <= M; ++m)
-	{
-		for (int n = 1; n <= N; ++n)
-		{
-			C(m,n) = min3(
-				C(m-1, n) + 2*avgDist,
-				C(m, n-1) + 2*avgDist,
-				C(m-1, n-1) + D(m-1, n-1)
-			);
-			
-			cout << C(m,n) << " ";
-		}
-		cout << endl;
-	}
-	
-	// gather results
-	int bestm = M, bestn = N;
-	for (int m = 3; m <= M; ++m) // start from 3 -> not interested in solutions with less than two em's
-		if (C(m,N) < C(bestm,bestn)) { bestm = m; bestn = N; }
-	for (int n = 3; n <= N; ++n)
-		if (C(M,n) < C(bestm,bestn)) { bestm = M; bestn = n; }
-	
-	// correlation check
-	if (bestm == 0 || bestn == 0)
-	{
-		cerr << "No correlation!" << endl;
-		return;
-	}
-	else
-	{
-		cout << "Best correlation: " << C(bestm,bestn) << endl;
-	}
-	
-	// trace back the optimal solution
-	int ms[3], ns[3]; // circular buffers
-	int m = bestm; int n = bestn;
-	int rnd = 0;
-	while (m > 0 && n > 0)
-	{
-		if (C(m,n) == C(m-1,n))
-			--m; // skip m
-		else if (C(m,n) == C(m,n-1))
-			--n; // skip n
-		else
-		{
-			// match! -> add to circular buffer
-			ms[rnd] = m-1;
-			ns[rnd] = n-1;
-			
-			rnd = (rnd + 1) % 3;
-			--m;
-			--n;
-		}
-	}
-	// in the circular buffer: first three matches
-	
-	cout << "We have this aligment:" << endl;
-	for (int i = 0; i < 3; ++i)
-	{
-		cout << "  " << ms[i] << " - " << ns[i];
-		cout << ", distance " << D(ms[i], ns[i]) << endl;
-	}
 }
 
 struct ScanItem
@@ -180,6 +94,13 @@ struct ScanItem
 
 void findBlobs(const Mat & mat, Blobs & blobs)
 {
+	/*
+	namedWindow("foo");
+	imshow("foo", mat);
+	waitKey(1000);
+	*/
+
+	cout << "depth: " << mat.depth() << ", type: " << mat.type() << ", chans: " << mat.channels() << endl;
 	list<ScanItem> scan, newscan;
 	for (int y = 0; y < mat.rows; ++y)
 	{
@@ -188,17 +109,21 @@ void findBlobs(const Mat & mat, Blobs & blobs)
 		int l = 0;
 		for (int x = 0; x < mat.cols; ++x)
 		{
+			// skip blanks
 			while (it != scan.end() && it->r < x)
 			{
 				blobs.push_back(it->blob);
 				++it;
 			}
 
+			// find the end of the white segment
 			while (x < mat.cols && row[x])
 				++x;
 
+			// if white segment found
 			if (row[l])
 			{
+				cout << "rowscan at " << l << ".." << x << "," << y << endl;
 				Blob cur = {(x+l-1)/2.0, y, x-l};
 				while (it != scan.end() && it->l < x)
 				{
@@ -207,15 +132,14 @@ void findBlobs(const Mat & mat, Blobs & blobs)
 				}
 				newscan.push_back(ScanItem(l,x-1,cur));
 			}
-			else
-			{
-				l = ++x;
-			}
+
+			l = ++x;
 		}
 		scan = newscan;
 	}
 
 	for (list<ScanItem>::const_iterator it = scan.begin(); it != scan.end(); ++it)
+
 	{
 		blobs.push_back(it->blob);
 	}
@@ -229,8 +153,9 @@ void getStars(const vector<string> & fn, vector<Stars *> & stars, const Options 
 		cout << "  * " << *it << ": ";
 
 		// load the image
-		Mat srcimg = imread(*it, CV_LOAD_IMAGE_GRAYSCALE);
-		Mat image;
+		Mat image = imread(*it, CV_LOAD_IMAGE_GRAYSCALE);
+		Mat srcimg;
+		resize(image, srcimg, Size(0,0), 0.25, 0.25);
 		threshold(srcimg, image, opt.threshold, 255, THRESH_BINARY);
 
 		// find the blobs
@@ -257,7 +182,7 @@ int main(int argc, char ** argv)
 
 	// some default options
 	Options opt;
-	opt.threshold = 48;
+	opt.threshold = 128;
 
 	// get the options
 	while (argv < end)
